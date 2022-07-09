3146ae7f0b88b73d290d39636c1d33557165b7c5
delarosa0305 <delarosa2381i@gmail.com>
2022-07-09 17:52:40 -0400

    created and populated tables

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,28 @@
-INSERT INTO department (id, name)
+INSERT INTO department (name)
 VALUES
-    ('', '')
+    ('Sales'),
+    ('Engineering'),
+    ('Finance'),
+    ('Legal')
+
+INSERT INTO role (title, salary, department_id)
+VALUES 
+    ('Salesperson', '$80,000', '1'),
+    ('Sales Manager', '$190,000', '1'),
+    ('Lead Engineer', '$200,000', '2'),
+    ('Software Engineer', '$140,000', '2'),
+    ('Accountant Manager', '$150,000', '3'),
+    ('Accountant', '$100,000', '3'),
+    ('Legal Team Lead', '$180,000', '4'),
+    ('Lawyer', '$130,000', '4')
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES 
+    ('jaerin', 'kim', '1', '1'),
+    ('qub', 'vud', '2', 'NULL'),
+    ('tud', 'val', '3', '3'),
+    ('duck', 'wellington', '4', 'NULL'),
+    ('dick', 'jasom', '5', '5'),
+    ('saq', 'fuol', '6', 'NULL'),
+    ('tom', 'lip', '6', '7'),
+    ('walter', 'shimmer', '6', 'NULL')
